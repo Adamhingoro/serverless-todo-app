6eca2cffff64b652f8331c6ed5ad5431e47838f9
Adam Hingoro <adam.abdulshakoor@gmail.com>
2020-04-04 18:08:44 +0500

    configured the config for client

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '2ufgv9xtxe'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'adamhingoro.eu.auth0.com',            // Auth0 domain
+  clientId: 'kDvpHoM5Td5dgdmOeRUaJRvm31TUo2qb',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
