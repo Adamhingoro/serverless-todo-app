3967b25372c171df3c93af206dafea3820e1b641
Adam Hingoro <adam.abdulshakoor@gmail.com>
2020-04-04 23:06:04 +0500

    Added new Application ID

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '2ufgv9xtxe'
+const apiId = 'qrg854yphj'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
