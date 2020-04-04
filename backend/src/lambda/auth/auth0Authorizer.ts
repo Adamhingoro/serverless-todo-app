933ed34669788b18f461835c5229e8fbbfe1a635
Adam Hingoro <adam.abdulshakoor@gmail.com>
2020-04-04 23:03:28 +0500

    Fixed the Oauth verifier

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,19 +1,12 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+import { verify } from 'jsonwebtoken'
 import { JwtPayload } from '../../auth/JwtPayload'
-import { jwkToPem } from 'jwk-to-pem'
 
+import { createLogger } from '../../utils/logger'
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://adamhingoro.eu.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -22,7 +15,6 @@ export const handler = async (
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
-
     return {
       principalId: jwtToken.sub,
       policyDocument: {
@@ -39,6 +31,7 @@ export const handler = async (
   } catch (e) {
     logger.error('User not authorized', { error: e.message })
 
+    console.log("Auth Event found..." , e.message);
     return {
       principalId: 'user',
       policyDocument: {
@@ -57,15 +50,7 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  const response = await Axios(jwksUrl);
-  const responseData = response.data;
-  const signingKey = responseData['keys'].find(key => key['kid'] === jwt['header']['kid']);
-  if (!signingKey) {
-    throw new Error('Invalid Signing key');
-  }
-  return verify(token, jwkToPem(signingKey), {algorithms: ['RS256']}) as JwtPayload;
+  return verify(token, process.env.OAUTH_CLIENT_SECRET , {algorithms: ['HS256']}) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
